6d0a785e00fa662b0d7b1433534d4e875d764d43
Josue <hounhouijosue1@gmail.com>
2026-03-13 15:49:14 +0100

    feat(content): adding image option to news

---
+++ content/src/middleware/multer.middleware.ts
@@ -0,0 +1,31 @@
+import multer from "multer";
+import path from "node:path";
+
+const MIME_TYPES: Record<string, string> = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "uploads");
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_').split('.')[0];
+        const extension = path.extname(file.originalname);
+        
+        callback(null, `${Date.now()}_${name}${extension}`);
+    }
+});
+
+export const upload = multer({ 
+    storage,
+    fileFilter: (req, file, callback) => {
+        if (MIME_TYPES[file.mimetype]) {
+            callback(null, true);
+        } else {
+            callback(new Error("Format de fichier non supporté"));
+        }
+    }
+});
