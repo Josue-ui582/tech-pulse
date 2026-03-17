1f866abed67eb6da07ac5d0c78f238c0012ac05c
Josue <hounhouijosue1@gmail.com>
2026-03-17 09:51:48 +0100

    feat(web): creating new article form to publish news

---
+++ web/src/schema/news.schema.ts
@@ -0,0 +1,21 @@
+import * as yup from 'yup';
+
+export const NEWS_CATEGORIES = ["Tech", "IA", "Dev"] as const;
+
+export const newsSchema = yup.object({
+  title: yup
+    .string()
+    .required('Le titre est requis')
+    .min(15, 'Le titre trop court'),
+  description: yup
+    .string()
+    .required('La description est requise')
+    .min(100, "La description est trop courte"),
+  imageUrl: yup
+    .mixed()
+    .required("Veuillez sélectionner une image"),
+  category: yup
+    .string()
+    .oneOf([...NEWS_CATEGORIES], 'Catégorie invalide')
+    .required('Sélectionnez une catégorie'),
+});
