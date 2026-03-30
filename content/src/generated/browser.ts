bccfef3e0523e8b6934ad82d4c881df75403448c
Josue <hounhouijosue1@gmail.com>
2026-03-30 19:52:49 +0100

    feat(content): replacing Users model by User

--- content/src/generated/browser.ts
+++ content/src/generated/browser.ts
@@ -23,7 +23,17 @@ export * from './enums.js';
  */
 export type News = Prisma.NewsModel
 /**
- * Model Users
+ * Model User
  * 
  */
-export type Users = Prisma.UsersModel
+export type User = Prisma.UserModel
+/**
+ * Model Account
+ * 
+ */
+export type Account = Prisma.AccountModel
+/**
+ * Model Session
+ * 
+ */
+export type Session = Prisma.SessionModel
