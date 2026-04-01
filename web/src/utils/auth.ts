ed35dbb522605bbe67b0a22f98eb1a0bf0c3aeb8
Josue <hounhouijosue1@gmail.com>
2026-04-01 11:36:14 +0100

    feat(web): initialization of guard logic

---
+++ web/src/utils/auth.ts
@@ -0,0 +1,9 @@
+export const getUser = () => {
+    if(typeof window === "undefined") return null;
+    const user = localStorage.getItem("user");
+    return user ? JSON.parse(user) : null;
+}
+
+export const isAuthentificated = () => {
+    return !!localStorage.getItem("token");
+}
