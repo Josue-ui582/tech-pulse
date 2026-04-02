b759e4ba41b6cbaaa7c6720faabb2110090896ab
Josue <hounhouijosue1@gmail.com>
2026-04-02 21:30:18 +0100

    refactor(web): imploving new card component

---
+++ web/public/icons/readMoreIcon.tsx
@@ -0,0 +1,7 @@
+export const ReadMoreIcon = () => {
+    return (
+        <svg className="w-4 h-4 transform group-hover:translate-x-1 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
+        </svg>
+    )
+}
