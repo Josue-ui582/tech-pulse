135d5cb29fd8b916f61899f79ecd791f2c274991
Josue <hounhouijosue1@gmail.com>
2026-04-03 10:01:41 +0100

    feat(web): redirecting user to /news page after click on start reading button on home page

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,11 +4,9 @@ import { Features } from "@/components/ui/feature";
 import { Footer } from "@/components/ui/footer";
 import { featuredNews } from "@/data/preview";
 import Image from "next/image";
-import { getUser } from "@/utils/auth";
+import { ReadMoreIcon } from "../../public/icons/readMoreIcon";
 
 export default function HomePage() {
-  const user = getUser();
-      console.log("USER ", user);
 
   return (
     <div className="min-h-screen bg-white">
@@ -71,9 +69,7 @@ export default function HomePage() {
 
                   <div className="mt-6 flex items-center gap-2 text-indigo-600 font-bold text-sm">
                     Lire l'article
-                    <svg className="w-4 h-4 transform group-hover:translate-x-1 transition-transform" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                    </svg>
+                    <ReadMoreIcon />
                   </div>
                 </div>
               </div>
