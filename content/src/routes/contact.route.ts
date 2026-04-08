7310511c4a47bdf1f50a66337d9b02e162f83a9f
Josue <hounhouijosue1@gmail.com>
2026-04-08 12:16:59 +0100

    feat(web): imploving contact route api

--- content/src/routes/contact.route.ts
+++ content/src/routes/contact.route.ts
@@ -3,6 +3,6 @@ import { contactController } from "../controllers/contact.controler.js";
 
 const router = Router();
 
-router.post("/contact", contactController);
+router.post("/", contactController);
 
 export default router;
