2741f14cc061bdf61f36b7359ee727561e39c9b1
Josue <hounhouijosue1@gmail.com>
2026-04-10 12:40:36 +0100

    feat(content): requiring 2fa code when login when isTwoFactorEnabled is true

--- content/src/controllers/2fa.controller.ts
+++ content/src/controllers/2fa.controller.ts
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import type { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { generate2FACode, verify2FACode } from "../services/2fa.service.js";
+import { prisma } from '../lib/pisma.js';
 
 const JWT_SECRET = process.env.JWT_SECRET_SECRET as string;
 
@@ -23,21 +24,36 @@ export const generateQRCode = async (req: Request, res: Response) => {
 
 export const verifyQRCode = async (req: Request, res: Response) => {
     const token = req.cookies.token;
+    const userId = req.cookies.temp_user_id;
     const { code } = req.body;
 
     if (!token) {
         return res.status(401).json({ message: "Token d'authentification manquant." });
     }
 
-    if (!code) {
-        return res.status(400).json({ message: "Code 2FA manquant." });
+    if (!userId || !code) {
+        return res.status(400).json({ message: "Session expirée ou code manquant." });
     }
 
     try {
-        const decoded: any = jwt.verify(token, JWT_SECRET);
-        const isValid = await verify2FACode(decoded.id, String(code));
+        const isValid = await verify2FACode(userId, String(code));
         if (isValid) {
-            res.json({ message: "2FA vérifié avec succès." });
+            const user = await prisma.user.findUnique({ where: { id: userId } });
+
+            const token = jwt.sign(
+                { id: user!.id, role: user!.role },
+                JWT_SECRET,
+                { expiresIn: '24h' }
+            );
+
+            res.clearCookie("temp_user_id");
+            res.cookie("token",
+                token, { 
+                    httpOnly: true, 
+                    maxAge: 24 * 60 * 60 * 1000 
+                });
+
+            res.json({ success: true, message: "Connexion réussie." });
         } else {
             res.status(400).json({ message: "Code 2FA invalide." });
         }
