a7ef2d8e7f9277de62598b9631c678123e136c98
Josue <hounhouijosue1@gmail.com>
2026-04-15 10:41:02 +0100

    feat(content): adding delete comment controller

--- content/src/controllers/comment.controller.ts
+++ content/src/controllers/comment.controller.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { addCommentService, updateCommentService } from "../services/comments.service.js";
+import { addCommentService, deleteCommentService, updateCommentService } from "../services/comments.service.js";
 
 export const addCommentController = async (req: Request, res: Response) => {
     const { token } = req.cookies;
@@ -43,10 +43,6 @@ export const updateCommentController = async (req: Request, res: Response) => {
         return res.status(400).json({ message: "Commentaire manquant" });
     }
 
-    if (!commentId) {
-        return res.status(400).json({ message: "ID de la news manquant" });
-    }
-
     try {
         const response = await updateCommentService(commentId as string, comment);
 
@@ -57,6 +53,31 @@ export const updateCommentController = async (req: Request, res: Response) => {
     } catch (error: any) {
         return res.status(500).json({
             message: "Erreur lors de la mise à jour de commentaire",
+            error: error.message 
+        });
+    }
+}
+
+export const deleteCommentController = async (req: Request, res: Response) => {
+    const { token } = req.cookies;
+    const { commentId } = req.query;
+
+    if (!token) {
+        return res.status(401).json({ message: "Authentification requise" });
+    }
+
+    if (!commentId) {
+        return res.status(400).json({ message: "Commentaire manquant" });
+    }
+
+    try {
+        await deleteCommentService(commentId as string);
+        return res.status(200).json({
+            message: "Commentaire supprimé avec succès"
+        })
+    } catch (error: any) {
+        return res.status(500).json({
+            message: "Erreur lors de la suppression du commentaire",
             error: error.message 
         });
     }
