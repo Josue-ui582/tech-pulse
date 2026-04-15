f16473fcf489e5a673ce5bd0b09959da244049b4
Josue <hounhouijosue1@gmail.com>
2026-04-15 10:34:35 +0100

    feat(content): adding update comment controller

--- content/src/controllers/comment.controller.ts
+++ content/src/controllers/comment.controller.ts
@@ -1,9 +1,9 @@
 import type { Request, Response } from "express";
-import { addCommentService } from "../services/comments.service.js";
+import { addCommentService, updateCommentService } from "../services/comments.service.js";
 
 export const addCommentController = async (req: Request, res: Response) => {
     const { token } = req.cookies;
-    const { newId } = req.params;
+    const { newId } = req.query;
     const { comment } = req.body;
 
     if (!token) {
@@ -29,3 +29,35 @@ export const addCommentController = async (req: Request, res: Response) => {
         });
     }
 };
+
+export const updateCommentController = async (req: Request, res: Response) => {
+    const { token } = req.cookies;
+    const { commentId } = req.query;
+    const { comment } = req.body;
+
+    if (!token) {
+        return res.status(401).json({ message: "Authentification requise" });
+    }
+
+    if (!commentId || comment.trim() === "") {
+        return res.status(400).json({ message: "Commentaire manquant" });
+    }
+
+    if (!commentId) {
+        return res.status(400).json({ message: "ID de la news manquant" });
+    }
+
+    try {
+        const response = await updateCommentService(commentId as string, comment);
+
+        return res.status(200).json({
+            message: "Commentaire mis à jour",
+            data: response
+        });
+    } catch (error: any) {
+        return res.status(500).json({
+            message: "Erreur lors de la mise à jour de commentaire",
+            error: error.message 
+        });
+    }
+}
