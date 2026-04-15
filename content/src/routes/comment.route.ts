0780e7c736fffce874d6132abfae6f5bcf831176
Josue <hounhouijosue1@gmail.com>
2026-04-15 11:55:58 +0100

    feat(content): creating get comment api route

--- content/src/routes/comment.route.ts
+++ content/src/routes/comment.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { addCommentController, deleteCommentController, updateCommentController } from "../controllers/comment.controller.js";
+import { addCommentController, deleteCommentController, getCommentsController, updateCommentController } from "../controllers/comment.controller.js";
 
 const router = Router();
 
 router.post("/comment", addCommentController);
 router.patch("/comment", updateCommentController);
 router.delete("/comment", deleteCommentController);
+router.get("/comment", getCommentsController)
 
 export default router;
